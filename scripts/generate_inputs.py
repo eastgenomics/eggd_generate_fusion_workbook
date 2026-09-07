1ad17705ce3a27dc6913af1496c1142f43f77d49
Emma-MacK <emma.mackenzie10@nhs.net>
2026-09-07 10:32:39 +0100

    update generate inputs set inputs to reflect new files

--- scripts/generate_inputs.py
+++ scripts/generate_inputs.py
@@ -56,9 +56,9 @@ def main() -> None:
         "starfusion_files": find_files(project_id, SF_PATTERN),
         "arriba_files": find_files(project_id, ARRIBA_PATTERN),
         "multiqc_files": find_files(project_id, MQC_PATTERN),
-        "SF_previous_runs_data": {"$dnanexus_link": "file-J18X5qj48F3V951v176vBq90"},
+        "SF_previous_runs_data": {"$dnanexus_link": "file-J6j4Fqj44Kkg9vYKYp2ZJZ3G"},
         "reference_sources": {"$dnanexus_link": "file-J18490j48F3b2zB4Z27KJJk4"},
-        "previous_positives": {"$dnanexus_link": "file-J0fqQ8848F3bfj1XjfBQbx8x"},
+        "previous_positives": {"$dnanexus_link": "file-J753pPQ44KkgG0B26fxGyXg3"},
     }
 
     with open("input.json", "w") as file:
